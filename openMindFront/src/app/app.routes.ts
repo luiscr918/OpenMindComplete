8716882ad6324eef7b7405c8215a5d5c0c6fae49
luiscr918 <144194654+luiscr918@users.noreply.github.com>
2026-02-21 20:28:03 -0500

    Revert "PaginasBasicas"

--- openMindFront/src/app/app.routes.ts
+++ openMindFront/src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Perfil } from './pages/perfil/perfil';
-import { Prestamos } from './pages/prestamos/prestamos';
-import { Contactos } from './pages/contactos/contactos';
-import { Nosotros } from './pages/nosotros/nosotros';
-import { Catalogo } from './pages/catalogo/catalogo';
 
-
-
-export const routes: Routes = [
-    { path: '', component:Home },
-    { path: 'perfil', component: Perfil },
-    { path: 'catalogo', component: Catalogo },
-    { path: 'prestamos', component: Prestamos },
-    { path: 'nosotros', component: Nosotros },
-    { path: 'contactos', component: Contactos },
-    { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
